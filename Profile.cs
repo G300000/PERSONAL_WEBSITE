254edad001fe839471dcf4bbc7df2198c98fa3aa
G300000 <Presheun5@gmail.com>
2025-12-23 21:32:53 +0800

    Add files via upload

---
+++ Profile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PersonalSite.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string Description { get; set; }
+    }
+}
